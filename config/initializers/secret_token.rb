cc382e58d209f92278a2521d34f9cc2cd3d3043c
Kevin Bongart <contact@kevinbongart.net>
2012-03-06 15:23:30 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trellolol::Application.config.secret_token = 'a6c897464a2e0f88ba4b5c2c92fe9c075a9987df8103b70ab7bc273fe8c8a4cc0147c97486b61a2ffcf156488e8cbd52992ad0a9b43d171cd3d864adefc87844'
